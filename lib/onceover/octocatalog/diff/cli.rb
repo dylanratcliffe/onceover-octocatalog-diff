7603263611f522c8d93f3112046ba4cb65b45577
Mitchell Chambers <mitchell.chambers@puppet.com>
2023-06-23 14:27:06 +1000

    replace double with single quotes

--- lib/onceover/octocatalog/diff/cli.rb
+++ lib/onceover/octocatalog/diff/cli.rb
@@ -44,7 +44,7 @@ class Onceover
               @results = []
 
               # Create r10k_cache_dir
-              logger.debug "Creating a common r10k cache"
+              logger.debug 'Creating a common r10k cache'
               r10k_cache_dir = Dir.mktmpdir('r10k_cache')
               r10k_config = {
                 'cachedir' => r10k_cache_dir,
@@ -68,12 +68,12 @@ class Onceover
 
               # Copy all of the factsets over in reverse order so that
               # local ones override vendored ones
-              logger.debug "Deploying vendored factsets"
+              logger.debug 'Deploying vendored factsets'
               deduped_factsets = repo.facts_files.reverse.inject({}) do |hash, file|
                 hash[File.basename(file)] = file
                 hash
               end
-              logger.info("Copy vendored factsets to control-repos")
+              logger.info('Copy vendored factsets to control-repos')
               deduped_factsets.each do |basename,path|
                 facts = JSON.load(File.read(path))
                 # Factsets are only read from todir, see command_args (--fact-file)
@@ -108,12 +108,12 @@ class Onceover
               frompuppetfile = "#{fromdir}/Puppetfile"
               from_content = File.read(frompuppetfile)
               new_content = from_content.gsub(/:control_branch/, "'#{opts[:from]}'")
-              File.open(frompuppetfile, "w") {|file| file.puts new_content }
+              File.open(frompuppetfile, 'w') {|file| file.puts new_content }
  
               topuppetfile = "#{todir}/Puppetfile"
               to_content = File.read(topuppetfile)
               new_content = to_content.gsub(/:control_branch/, "'#{opts[:to]}'")
-              File.open(topuppetfile, "w") {|file| file.puts new_content }
+              File.open(topuppetfile, 'w') {|file| file.puts new_content }
 
 
               # Deploy Puppetfile in from
@@ -124,7 +124,7 @@ class Onceover
                 if exit_status.exitstatus != 0
                   STDOUT.puts stdout.read
                   STDERR.puts stderr.read
-                  abort "R10k encountered an error, see the logs for details"
+                  abort 'R10k encountered an error, see the logs for details'
                 end
               end
 
@@ -143,13 +143,13 @@ class Onceover
                 Thread.new do
                   until @queue.empty?
                     test = @queue.shift
-                    
+
                     logger.info "Preparing environment for #{test.classes[0].name} on #{test.nodes[0].name}"
 
                     # To enable parrallel testing, each role / node pair is allocated a thread.
-                    # To support multiple threads compiling catalogs using the same `to` and `from` environments 
-                    # we must adopt node classification strategy that will not leak between threads. Site.pp is 
-                    # common amongst each thread, using an `include <role_class>` declaration would leak into other 
+                    # To support multiple threads compiling catalogs using the same `to` and `from` environments
+                    # we must adopt node classification strategy that will not leak between threads. Site.pp is
+                    # common amongst each thread, using an `include <role_class>` declaration would leak into other
                     # threads resulting nodes applying resources from other role classes.
                     #
                     # A dedicated ENC script per role/node pair ensures classification freshness without complex logic.
@@ -160,21 +160,21 @@ class Onceover
                     class_name = test.classes[0].name
                     control_repos = [fromdir, todir]
                     safe_class = class_name.gsub(/::/, "_")
-                    
+
                     # Create an ENC script for the current thread's target node and role class
                     # This ensures this thread will only apply this role class.
-                    control_repos.each do | file_name |
-                      tempfile = File.open("#{file_name}/scripts/#{test.nodes[0].name}-#{safe_class}.sh", "w")
+                    control_repos.each do |file_name|
+                      tempfile = File.open("#{file_name}/scripts/#{test.nodes[0].name}-#{safe_class}.sh", 'w')
                       tempfile.puts "echo '---\nclasses:\n  #{class_name}:'"
                       tempfile.close
                       File.chmod(0744,"#{file_name}/scripts/#{test.nodes[0].name}-#{safe_class}.sh")
                     end
 
-                    
-                    logger.debug "Getting Puppet binary"
+
+                    logger.debug 'Getting Puppet binary'
                     binary = `which puppet`.chomp
 
-                    logger.debug "Running Octocatalog diff"
+                    logger.debug 'Running Octocatalog diff'
                     logger.info "Compiling catalogs for #{test.classes[0].name} on #{test.nodes[0].name}"
 
                     command_prefix = ENV['BUNDLE_GEMFILE'] ? 'bundle exec ' : ''
@@ -231,13 +231,13 @@ class Onceover
                 puts "#{"Errors:".bold}\n#{result[:stderr]}\n" if result[:exit_status] == 1
                 puts ""
               end
-              logger.info "Cleanup temp environment directories"
+              logger.info 'Cleanup temp environment directories'
               logger.debug "Cleanup temp from directory created at #{fromdir}"
               FileUtils.rm_r(fromdir)
               logger.debug "Cleanup temp to directory created at #{todir}"
               FileUtils.rm_r(todir)
 
-              logger.info "Removing temporary build cache"    
+              logger.info 'Removing temporary build cache'
               logger.debug "Processing removal: #{r10k_cache_dir}"
               FileUtils.rm_r(r10k_cache_dir)
             end
