68bb57235a2d24c19954b2793c723e8e89b78501
Mitchell Chambers <mitchell.chambers@puppet.com>
2023-07-13 14:42:55 +1000

    enable cleanup of temp dirs

--- lib/onceover/octocatalog/diff/cli.rb
+++ lib/onceover/octocatalog/diff/cli.rb
@@ -253,8 +253,8 @@ class Onceover
 
                 tp.set :max_width, 200
                 tp @results, 
-                { node: lambda { |result| result[:test][:node]} }, 
                 { class: lambda { |result| result[:test][:class]} }, 
+                { node: lambda { |result| result[:test][:node]} }, 
                 { status: lambda { |result| states[result[:exit_status]]} }, 
                 { add: lambda { |result| result[:stdout].scan(/\+ /).length} }, 
                 { remove: lambda { |result| result[:stdout].scan(/- /).length} } 
@@ -265,15 +265,15 @@ class Onceover
               
               logger.info 'Cleanup temp environment directories'
               logger.debug "Processing removal: #{fromdir}"
-              #FileUtils.rm_r(fromdir)
+              FileUtils.rm_r(fromdir)
               logger.debug "Processing removal: #{todir}"
-              #FileUtils.rm_r(todir)
+              FileUtils.rm_r(todir)
 
               logger.info 'Removing temporary build cache'
               logger.debug "Processing removal: #{r10k_cache_dir}"
-              #FileUtils.rm_r(r10k_cache_dir)
+              FileUtils.rm_r(r10k_cache_dir)
               logger.debug "Processing removal: #{environment_dir}"
-              #FileUtils.rm_r(environment_dir)
+              FileUtils.rm_r(environment_dir)
             end
           end
         end
